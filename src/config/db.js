5a3061410ec17ccc54ee8456ef90f76c90c33fa6
SemicolonCoders <pragatis924@gmail.com>
2024-06-15 17:03:02 +0530

    first commit

---
+++ src/config/db.js
@@ -0,0 +1,15 @@
+import dot from "dotenv"
+
+dot.config();
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("Connected to DB")
+    } catch (error) {
+        console.log("Error connecting to DB", error)
+    }
+}
+
+export default connectDB;
