3bb09d2ba1b46c70ea7617c2120c056ab8b9b850
SemicolonCoders <pragatis924@gmail.com>
2024-06-16 22:19:44 +0530

    Resolved merge conflicts

--- src/routes/product.routes.js
+++
@@ -1,7 +0,0 @@
-// import express from 'express';
-// import {addProducts} from '../controllers/product.controller';
-// const productRouter = express.Router();
-
-// productRouter.post('/addproducts', addProducts);
-
-// export default productRouter;
