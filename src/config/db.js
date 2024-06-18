82e500e1f7e8d51ebda9e3775765d26a26875614
Pragati Srivastava <138607997+SemicolonCoders@users.noreply.github.com>
2024-06-18 18:58:58 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,21 @@
-import dot from "dotenv"
-
+// Import the dotenv module and load environment variables
+import dot from "dotenv";
 dot.config();
+
+// Import mongoose for MongoDB interaction
 import mongoose from "mongoose";
 
+// Function to connect to MongoDB database
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI)
-        console.log("Connected to DB")
+        // Connect to MongoDB using the MONGODB_URI environment variable
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Connected to DB");
     } catch (error) {
-        console.log("Error connecting to DB", error)
+        // Handle errors if connection fails
+        console.log("Error connecting to DB", error);
     }
 }
 
+// Export the connectDB function
 export default connectDB;
