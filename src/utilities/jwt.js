1b82f6fcf32f0f0d3135288b467fc47c92ee4599
Pragati Srivastava <138607997+SemicolonCoders@users.noreply.github.com>
2024-06-18 18:55:42 +0530

    Update jwt.js

--- src/utilities/jwt.js
+++ src/utilities/jwt.js
@@ -1,15 +1,21 @@
+// Import the jwt module
 import jwt from "jsonwebtoken";
 
+// Function to create a token with provided data
 const createToken = (data) => {
+    // Sign the data with the secret key and set expiration time to 1 day
     return jwt.sign(data, process.env.JWT_SECRET_KEY, {
-        expiresIn : "1d"
-    })
+        expiresIn: "1d"
+    });
 }
 
+// Function to verify a token
 const verifyToken = (token) => {
+    // Verify the token with the secret key
     return jwt.verify(token, process.env.JWT_SECRET_KEY);
 }
 
+// Export the createToken and verifyToken functions
 export {
     createToken,
     verifyToken
